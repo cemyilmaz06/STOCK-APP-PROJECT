4d435e2c4a8f72c4c6ce19bf60ea3d96ec97091f
cem <cemyilmaztech@gmail.com>
2024-10-13 16:56:21 +0300

    register

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -4,26 +4,44 @@ const authSlice = createSlice({
   name: "auth",
 
   initialState: {
-    token:"",
-    username:"",
-    loading:"",
-    error:""
+    token: "",
+    username: "",
+    loading: "",
+    error: "",
   },
   reducers: {
-    fetchStart: (state)=>{
-      state.loading=true
+    fetchStart: (state) => {
+      state.loading = true
     },
-    loginSuccess: (state,{payload})=>{
-      state.token=payload.token
-      state.username=payload.user.username
-      state.loading=false
+    loginSuccess: (state, { payload }) => {
+      state.token = payload.token
+      state.username = payload.user.username
+      state.loading = false
     },
-    fetchFail: (state)=>{
-      state.loading=false
-      state.error=true
-    }
+    //? Register işlemi için ayrı bir action creator fonksiyonun yazılması
+    //? Register ile Login arasında bazı farklılıklar olduğu için ayrı bir fonksiyon yazıldı.
+    registerSuccess: (state, { payload }) => {
+      state.loading = false
+      state.username = payload.data.username
+      state.token = payload.token
+    },
+    logoutSuccess: (state) => {
+      state.loading = false
+      state.username = ""
+      state.token = ""
+    },
+    fetchFail: (state) => {
+      state.loading = false
+      state.error = true
+    },
   },
 })
 
-export const {fetchFail,fetchStart,loginSuccess} = authSlice.actions
+export const {
+  fetchStart,
+  loginSuccess,
+  registerSuccess,
+  logoutSuccess,
+  fetchFail,
+} = authSlice.actions
 export default authSlice.reducer
