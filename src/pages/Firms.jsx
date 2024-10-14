faa92dabde396bbd9dcf1a83de1ae99260c8fc25
cem <cemyilmaztech@gmail.com>
2024-10-14 09:03:51 +0300

    getFirms

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
 
 const Firms = () => {
+  const {token}=useSelector(state=>state.auth)
+const getFirm=async()=>{
+
+  try {
+    const {data}= await axios(`${process.env.REACT_APP_BASE_URL}/firms`,{
+      headers: {Authorization:  `Token ${token}`},
+    })
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+useEffect(()=>{
+getFirm()
+},[])
+
   return (
     <div>Firms</div>
   )
