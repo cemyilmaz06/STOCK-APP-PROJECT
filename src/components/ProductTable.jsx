bd2d1802599133221895e092a67291e4e05d98dd
cem <cemyilmaztech@gmail.com>
2024-10-19 14:12:42 +0300

    table

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -3,34 +3,42 @@ import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: '_id', headerName: 'ID', width: 90 },
   {
-    field: 'firstName',
-    headerName: 'First name',
+    field: 'categories',
+    headerName: 'Categories',
     width: 150,
     editable: true,
   },
   {
-    field: 'lastName',
-    headerName: 'Last name',
+    field: 'brands',
+    headerName: 'Brands',
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
+    field: 'name',
+    headerName: 'Name',
     type: 'number',
     width: 110,
     editable: true,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+    field: 'stock',
+    headerName: 'Stock',
+    type: 'number',
+    width: 110,
+    editable: true,
   },
+//   {
+//     field: 'actions',
+//     type: 'actions',
+//     getActions: (params) => [
+//       <GridActionsCellItem icon={...} onClick={...} label="Delete" />,
+//       <GridActionsCellItem icon={...} onClick={...} label="Print" showInMenu />,
+//     ]
+//   }
+  
 ];
 
 const rows = [
@@ -41,7 +49,7 @@ const rows = [
   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 8, lastName: 'Franceska', firstName: 'Rossini', age: 36 },
   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
 ];
 
@@ -59,7 +67,6 @@ export default function ProductTable() {
           },
         }}
         pageSizeOptions={[5]}
-        checkboxSelection
         disableRowSelectionOnClick
       />
     </Box>
