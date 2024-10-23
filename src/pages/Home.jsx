d296a39725f2e3571a88baa024f5e8cf2015c092
cem <cemyilmaztech@gmail.com>
2024-10-24 00:52:10 +0300

    KPI card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import KPICard from '../components/KPICard'
 import Charts from '../components/Charts'
-
+import useStockRequests from "../services/useStockRequests"
 const Home = () => {
+  const{getStock}=useStockRequests()
+  useEffect(()=>{
+getStock("purchases")
+getStock("sales")
+  },[])
   return (
     <div>
       <KPICard/>
