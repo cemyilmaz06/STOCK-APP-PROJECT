0dbbe2bbd88404441a84c29633b3250d5cfc7ff2
cem <cemyilmaztech@gmail.com>
2024-10-24 01:22:12 +0300

    area chart

--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,6 +1,8 @@
 "use client"
 
+import { Stack } from "@mui/material";
 import { AreaChart } from "@tremor/react";
+import { useSelector } from "react-redux";
 
 
 
@@ -68,16 +70,47 @@ const chartdata = [
 ]
 const dataFormatter=(number)=>
   `${Intl.NumberFormat("us").format(number).toString()}`
-const Charts = () => (
-  <AreaChart
-    className="h-80"
-    data={chartdata}
-    index="date"
-    categories={["SolarPanels", "Inverters"]}
-    colors={["indigo", "rose"]}
-    valueFormatter={dataFormatter}
-   yAxisWidth={60}
-  />
-)
+const Charts = () => {
+  const { sales, purchases } = useSelector((state) => state.stock)
 
-export default Charts;
+  console.log(sales)
+
+  const salesData = sales.map((sale) => ({
+    salesAmount: sale.amount,
+    date: new Date(sale.createdAt).toLocaleDateString("tr-TR"),
+  }))
+
+  const purchasesData = purchases.map((pur) => ({
+    purAmount: pur.amount,
+    date: new Date(pur.createdAt).toLocaleDateString("tr-TR"),
+  }))
+
+  return (
+    <Stack
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection="column"
+      flexWrap={"wrap"}
+    >
+      <AreaChart
+        className="h-80"
+        data={salesData}
+        index="date"
+        categories={["salesAmount"]}
+        colors={["indigo"]}
+        valueFormatter={dataFormatter}
+        yAxisWidth={80}
+      />
+      <AreaChart
+        className="h-80"
+        data={purchasesData}
+        index="date"
+        categories={["purAmount"]}
+        colors={["red"]}
+        valueFormatter={dataFormatter}
+        yAxisWidth={80}
+      />
+    </Stack>
+  )
+}
+export default Charts
